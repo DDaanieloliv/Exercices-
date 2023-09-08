efcd89cae85b44c073b300571ede0f845a55debd
DDaanieloliv <daniel0333v@gmail.com>
2023-09-08 14:54:56 -0300

    .

--- form_/pwabuilder-sw.js
+++ form_/pwabuilder-sw.js
@@ -1,54 +1,79 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+// // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-const CACHE = "pwabuilder-offline-page";
+// TODO: Install workBox-build from a command prompt
+// TODO:   npm install workbox-build
+const workboxBuild = require('workbox-build');
+const SRC_DIR = 'src';
+const BUILD_DIR = '{1:dist}';
+const SW = 'sw.js';
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const input ={
+  swSrc: `${SRC_DIR}/${SW}`,
+  swDest: `${BUILD_DIR}/${SW}`,
+  globDirectory: BUILD_DIR,
+  globPatterns: [
+    '**/*.{js,png,ico,svg,html,css}',
+    'assets/**/*'
+  ],
+  globIgnores: ['sw.js'],
+  maximumFileSizeToCacheInBytes: 4000000
+};
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+workboxBuild.injectManifest(input).then(() =>{
+  console.log(`The service worker ${BUILD_DIR}/${SW} has been injected with a precache list.`);
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+// const CACHE = "pwabuilder-offline-page";
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-        if (preloadResp) {
-          return preloadResp;
-        }
+// // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+// const offlineFallbackPage = "ToDo-replace-this-name.html";
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
+// self.addEventListener("message", (event) => {
+//   if (event.data && event.data.type === "SKIP_WAITING") {
+//     self.skipWaiting();
+//   }
+// });
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+// self.addEventListener('install', async (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE)
+//       .then((cache) => cache.add(offlineFallbackPage))
+//   );
+// });
+
+// if (workbox.navigationPreload.isSupported()) {
+//   workbox.navigationPreload.enable();
+// }
+
+// workbox.routing.registerRoute(
+//   new RegExp('/*'),
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: CACHE
+//   })
+// );
+
+// self.addEventListener('fetch', (event) => {
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith((async () => {
+//       try {
+//         const preloadResp = await event.preloadResponse;
+
+//         if (preloadResp) {
+//           return preloadResp;
+//         }
+
+//         const networkResp = await fetch(event.request);
+//         return networkResp;
+//       } catch (error) {
+
+//         const cache = await caches.open(CACHE);
+//         const cachedResp = await cache.match(offlineFallbackPage);
+//         return cachedResp;
+//       }
+//     })());
+//   }
+// });
